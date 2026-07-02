d801e1abc8ecc31895a7d6747fd103bc7f8377f1
Dasha Kuracheva <dkuraceva2004@gmail.com>
2026-07-02 19:48:01 +0300

    Main window GUI

---
+++ Test_Task/mainwindow.cpp
@@ -0,0 +1,51 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
+{
+    //формрование ЦЕНТРАЛЬНОГО ВИДЖЕТА
+    centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    //формирование горизонтального слоя
+    mainLayout = new QHBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(50, 50, 50, 50);
+    mainLayout->setSpacing(100);
+
+
+    //левая часть
+    id = new QLabel("ID станции:");
+
+    listWidget = new QListWidget(this);
+
+    item1 = new QListWidgetItem(QIcon(":/new/prefix1/home.png"), "Главная");
+    listWidget->addItem(item1);
+    item2 = new QListWidgetItem(QIcon(":/new/prefix1/setting.png"), "Конфигурация");
+    listWidget->addItem(item2);
+    item3 = new QListWidgetItem(QIcon(":/new/prefix1/diagnostis.png"), "Диагностика");
+    listWidget->addItem(item3);
+    item4 = new QListWidgetItem(QIcon(":/new/prefix1/update.png"), "Обновление ПО");
+    listWidget->addItem(item4);
+    item5 = new QListWidgetItem(QIcon(":/new/prefix1/analytics.png"), "Аналитика");
+    listWidget->addItem(item5);
+
+    //правая чассть
+    stackedWidget = new QStackedWidget(this);
+    page1 = new QLabel("<h1>Контент 1</h1>", this); page1->setAlignment(Qt::AlignCenter);
+    page2 = new QLabel("<h1>Контент 2</h1>", this); page2->setAlignment(Qt::AlignCenter);
+    page3 = new QLabel("<h1>Контент 3</h1>", this); page1->setAlignment(Qt::AlignCenter);
+    page4 = new QLabel("<h1>Контент 4</h1>", this); page2->setAlignment(Qt::AlignCenter);
+    page5 = new QLabel("<h1>Контент 5</h1>", this); page1->setAlignment(Qt::AlignCenter);
+    stackedWidget->addWidget(page1);
+    stackedWidget->addWidget(page2);
+    stackedWidget->addWidget(page3);
+    stackedWidget->addWidget(page4);
+    stackedWidget->addWidget(page5);
+
+    mainLayout->addWidget(listWidget, 1);
+    mainLayout->addWidget(stackedWidget, 4);
+
+    connect(listWidget, &QListWidget::currentRowChanged, stackedWidget, &QStackedWidget::setCurrentIndex);
+    listWidget->setCurrentRow(0);
+}
+
+MainWindow::~MainWindow(){};
---
+++ Test_Task/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtWidgets>
+#include <QDockWidget>
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+     QWidget *centralWidget;
+     QHBoxLayout *mainLayout;
+     QListWidget *listWidget;
+     QLabel *id, *page1,*page2,*page3,*page4,*page5;
+     QListWidgetItem *item1, *item2, *item3, *item4, *item5;
+     QStackedWidget *stackedWidget;
+
+
+};
+#endif // MAINWINDOW_H
